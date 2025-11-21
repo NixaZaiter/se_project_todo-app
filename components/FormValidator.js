76226ef3a4fcc6a805614015488ad9ba519fd48e
Nixa Zaiter <linkclone30@gmail.com>
2025-11-20 22:32:11 -0600

    Feat: Add "Todo" class and refactor todo card generation

---
+++ components/FormValidator.js
@@ -0,0 +1,15 @@
+class FormValidator {
+  constructor(config, form) {
+    this._config = config;
+    this._form = form;
+  }
+  _formProcessor() {}
+
+  _submitValidate() {}
+
+  _eventHandlers() {}
+
+  enableValidation() {}
+
+  disableSubmitBtn() {}
+}
